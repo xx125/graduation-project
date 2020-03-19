4d4844d4f5cb21389dbf35ce6cc805ce02e39984
yhL125 <yhl125@yhL125deMacBook-Pro.local>
2020-03-19 22:00:56 +0800

    查看评价功能，推荐/吐槽

--- mpvue-koa/my-project-node/controllers/goods/index.js
+++ mpvue-koa/my-project-node/controllers/goods/index.js
@@ -4,14 +4,41 @@ const { mysql } = require('../../mysql')
 async function detailAction (ctx) {
   const goodsId = ctx.query.id
   const openId = ctx.query.openId
+
   // 商品信息
   const info = await mysql('food').where({
     'id': goodsId
   }).select()
-  ctx.body = {
-    'info': info[0] || []
-  }
   // console.log(info)
+
+  // 商品评论
+  const allcomments = await mysql('comments').where({
+    'foodId': goodsId
+  }).select()
+  // console.log(comments)
+
+  const goodcomm = await mysql('comments').where('rateType', 'like', '推荐').where({
+    'foodId': goodsId
+  }).select()
+  // console.log(goodcomm)
+
+  const badcomm = await mysql('comments').where('rateType', 'like', '吐槽').where({
+    'foodId': goodsId
+  }).select()
+  console.log(badcomm)
+
+  // 商品评论对应的图片
+  // const commImg = await mysql('comment_image').where({
+  //   'commentId': commentId
+  // }).select()
+  // console.log(commImg)
+
+  ctx.body = {
+    'info': info[0] || [],
+    'allcomments': allcomments,
+    'goodcomm': goodcomm,
+    'badcomm': badcomm
+  }
 } 
 
 module.exports = {
--- mpvue-koa/my-project/src/pages/goods/index.vue
+++ mpvue-koa/my-project/src/pages/goods/index.vue
@@ -1,40 +1,34 @@
 <template>
-  <!-- <transition name="move"> -->
-  <!-- <div class="detailWrapper" ref="detailWrapper" v-show="showDetail"> -->
   <div class="detailWrapper">
-    <div class="foodDetail">
-      <div class="back" @click="showToggle()">
-        <i class="icon-arrow_lift"></i>
-      </div>
-      <img :src="info.image" class="image" />
-      <div class="info">
-        <div class="title">{{info.name}}</div>
-        <div class="desc">
-          <span>月售{{info.sellCount}}</span>
-          <span>好评率{{info.rating}}%</span>
-        </div>
-        <div class="price">
-          <span class="unit">￥</span>
-          {{info.price}}
-          <span class="oldPrice" v-show="info.oldPrice">￥{{info.oldPrice}}</span>
-        </div>
-        <div class="shopCart">
-          <transition name="fade">
-            <!-- <div class="text" @click="addCart($event)" v-show="!food.count">加入购物车</div> -->
-            <div class="text">+</div>
-          </transition>
-        </div>
-        <!-- <cartcontrol :food="food"></cartcontrol> -->
-      </div>
-      <div class="divider"></div>
+    <img :src="info.image" class="image" />
+    <div class="info">
+      <div class="title">{{info.name}}</div>
+      <div class="collection"></div>
       <div class="desc">
-        <div class="title">商品介绍</div>
-        <div class="content">{{info.description}}</div>
+        <span>月售{{info.sellCount}}</span>
+        <span>好评率{{info.rating}}%</span>
       </div>
-      <div class="divider"></div>
+      <div class="price">
+        <span class="unit">￥</span>
+        {{info.price}}
+        <span class="oldPrice" v-show="info.oldPrice">￥{{info.oldPrice}}</span>
+      </div>
+      <div class="shopCart">
+        <transition name="fade">
+          <!-- <div class="text" @click="addCart($event)" v-show="!food.count">加入购物车</div> -->
+          <div class="text">+</div>
+        </transition>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="desc">
+      <div class="title">商品介绍</div>
+      <div class="content">{{info.description}}</div>
+    </div>
+    <div class="divider"></div>
 
-      <!-- 评价 -->
-      <!-- <div class="evaluation">
+    <!-- 评价 -->
+    <!-- <div class="evaluation">
           <div class="title">
             商品评价
           </div>
@@ -64,15 +58,106 @@
               </li>
             </ul>
           </div>
-      </div>-->
+    </div>-->
+
+    <div class="comment-list">
+      <div>商品评价</div>
+      <div class="tag">
+        <div @click="good(index)" :class="{choose:gooddisplay==1}">推荐</div>
+        <div @click="bad(index)" :class="{choose:baddisplay==1}">吐槽</div>
+      </div>
+
+      <div v-show="gooddisplay==1">
+        <div class="item-c" v-for="(item, index) in goodcomm" :key="index">
+          <div class="item-l">
+            <img :src="item.useravatar" />
+          </div>
+          <div class="item-r">
+            <div class="h-r">
+              <div class="r-t">
+                <span class="name">{{item.username}}</span>
+                <span class="date">{{item.rateTime}}</span>
+              </div>
+              <!-- <div class="r-b">
+                  <div class="b-l">
+                    <i class="icon mt-star-s" v-for="(itx, idx) in stars" :key="idx"></i>
+                  </div>
+              </div>-->
+            </div>
+            <div class="r-comtent">
+              <span>{{item.text}}</span>
+            </div>
+            <div class="r-imgs">
+              <img :src="item.commImg" />
+              <span :class="item.rateType=='推荐' ? 'good' : 'bad'">{{item.rateType}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-show="baddisplay==1">
+        <div class="item-c" v-for="(item, index) in badcomm" :key="index">
+          <div class="item-l">
+            <img :src="item.useravatar" />
+          </div>
+          <div class="item-r">
+            <div class="h-r">
+              <div class="r-t">
+                <span class="name">{{item.username}}</span>
+                <span class="date">{{item.rateTime}}</span>
+              </div>
+              <!-- <div class="r-b">
+                  <div class="b-l">
+                    <i class="icon mt-star-s" v-for="(itx, idx) in stars" :key="idx"></i>
+                  </div>
+              </div>-->
+            </div>
+            <div class="r-comtent">
+              <span>{{item.text}}</span>
+            </div>
+            <div class="r-imgs">
+              <img :src="item.commImg" />
+              <span :class="item.rateType=='推荐' ? 'good' : 'bad'">{{item.rateType}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div v-show="alldisplay==1">
+        <div class="item-c" v-for="(item, index) in allcomments" :key="index">
+          <div class="item-l">
+            <img :src="item.useravatar" />
+          </div>
+          <div class="item-r">
+            <div class="h-r">
+              <div class="r-t">
+                <span class="name">{{item.username}}</span>
+                <span class="date">{{item.rateTime}}</span>
+              </div>
+              <!-- <div class="r-b">
+                  <div class="b-l">
+                    <i class="icon mt-star-s" v-for="(itx, idx) in stars" :key="idx"></i>
+                  </div>
+              </div>-->
+            </div>
+            <div class="r-comtent">
+              <span>{{item.text}}</span>
+            </div>
+            <div class="r-imgs">
+              <img :src="item.commImg" />
+              <span :class="item.rateType=='推荐' ? 'good' : 'bad'">{{item.rateType}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <!-- meituan 底部购物车代码 -->
-    <div class="footer">
-    <!-- <div class="footer-c" v-if="pageIndex === 0"> -->
+    <div class="footer" @click="showType">
+      <!-- <div class="footer-c" v-if="pageIndex === 0"> -->
       <!-- <div class="c-t">
-        <span>{{reduceTip}}</span>
-      </div> -->
+          <span>{{reduceTip}}</span>
+      </div>-->
       <div class="footer-wrapper">
         <div class="container">
           <!-- <span class="price" v-if="totalPrice > 0 || productCount > 0"> -->
@@ -91,7 +176,7 @@
           class="container-price"
           :style="{'background-color': btnTitle === '去结算' ? '#F0D179' : '#2F2F2F'}"
           @click="orderClick"
-        > -->
+        >-->
         <div class="container-price">
           <!-- <span :style="{color: btnTitle === '去结算' ? '#333' : '#666'}">{{btnTitle}}</span> -->
           <span>15元起送</span>
@@ -101,34 +186,59 @@
         <!-- <img
           mode="widthFix"
           :src="productCount > 0 ? '../../../static/images/ic_menu_shoping_nor.png' : '../../../static/images/ic_menu_shoping_pressed.png'"
-        /> -->
-        <img src="../../../static/images/ic_menu_shoping_nor.png" >
+        />-->
+        <img src="../../../static/images/ic_menu_shoping_nor.png" />
         <span v-if="productCount > 0">{{productCount}}</span>
       </div>
     </div>
+
+    <!-- 购物车弹出层 -->
+    <div class="pop" v-show="showpop" @click="showType"></div>
+    <div class="attr-pop" :class="[showpop ? 'fadeup' : 'fadedown']">
+      <div class="top">
+        <div class="close">
+          <img src="../../../static/images/del-address.png" alt />
+          <span>清空购物车</span>
+        </div>
+        <div class="shopcart">
+          <div class="left">
+            <img :src="info.image" alt />
+          </div>
+          <div class="right">
+            <div>
+              <p>{{info.name}}</p>
+              <p>¥{{info.price}}</p>
+              <div class="count">
+                <div class="cut" @click="reduce">-</div>
+                <input type="text" class="number" v-model="number" disabled="false" />
+                <div class="add" @click="add">+</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <!-- </transition> -->
 </template>
 
 <script>
-// import '../../filter/time.js'
 import BScroll from "better-scroll";
 import { get, post } from "../../utils";
-// import cartcontrol from '../../components/cartcontrol/cartcontrol'
 
 export default {
-  // components: {
-  //   cartcontrol
-  // },
-  // props: {
-  //   food: Object
-  // },
   data() {
     return {
       id: "",
       openId: "",
       info: {},
-      count: 0
+      showpop: false,
+      number: 0,
+      gooddisplay: 0,
+      baddisplay: 0,
+      alldisplay:1,
+      goodcomm:[],
+      badcomm:[],
+      allcomments: []
       // showDetail: false,
       // classifyArr: [{
       //   name: '全部',
@@ -144,75 +254,77 @@ export default {
       //   active: false
       // }],
       // evelflag: true
-    };
+    }
   },
   computed: {
-  //   evelArr() {
-  //     let selectIndex = 0
-  //     this.classifyArr.forEach((data, index) => {
-  //       if (data.active) {
-  //         selectIndex = index
-  //       }
-  //     })
-  //     if (this.detailWrapper) {
-  //       this.$nextTick(() => {
-  //         this.detailWrapper.refresh()
-  //       })
-  //     }
-  //     return selectIndex ? this.food.ratings.filter((data) => this.evelflag ? data.rateType === selectIndex - 1 && data.text : data.rateType === selectIndex - 1) : this.food.ratings.filter((data) => this.evelflag ? data.text : true)
-  //   }
+    //   evelArr() {
+    //     let selectIndex = 0
+    //     this.classifyArr.forEach((data, index) => {
+    //       if (data.active) {
+    //         selectIndex = index
+    //       }
+    //     })
+    //     if (this.detailWrapper) {
+    //       this.$nextTick(() => {
+    //         this.detailWrapper.refresh()
+    //       })
+    //     }
+    //     return selectIndex ? this.food.ratings.filter((data) => this.evelflag ? data.rateType === selectIndex - 1 && data.text : data.rateType === selectIndex - 1) : this.food.ratings.filter((data) => this.evelflag ? data.text : true)
+    //   }
     totalPrice() {
-      var price = 0
+      var price = 0;
       // this.foods.map(item => price += item.totalPrice)
       return parseFloat(price).toFixed(1);
     },
     productCount() {
-      var count = 0
+      var count = 0;
       // this.foods.map(item => count += item.count)
-      return count
+      return count;
     },
     reduceTip() {
       // var content = this.shopInfo.prompt_text
-      var content = '15元起送'
-      var price = 0
+      var content = "15元起送";
+      var price = 0;
       // this.foods.map(item => price += item.totalPrice)
-      if (price <= 0) return content
+      if (price <= 0) return content;
       if (price < this.shopInfo.min_price) {
-        var value = parseFloat(this.shopInfo.min_price - price).toFixed(1)
-        return `还差 ${value}元 就能起送`
+        var value = parseFloat(this.shopInfo.min_price - price).toFixed(1);
+        return `还差 ${value}元 就能起送`;
       }
-      var activity_info = this.shopInfo.activity_info
+      var activity_info = this.shopInfo.activity_info;
       for (let i = 0; i < activity_info.length; i++) {
         const item = activity_info[i];
         if (price < item.priceLower) {
-          var str = parseFloat(item.priceLower - price).toFixed(1)
+          var str = parseFloat(item.priceLower - price).toFixed(1);
           if (i === 0) {
-            this.changeReduceFeeDataMut(0.0)
-            return `再买 ${str}元 可减 ${item.reduce}元 [去凑单]`
+            this.changeReduceFeeDataMut(0.0);
+            return `再买 ${str}元 可减 ${item.reduce}元 [去凑单]`;
           } else {
             var perItem = activity_info[i - 1];
-            this.changeReduceFeeDataMut(perItem.reduce)
-            return `已减${perItem.reduce}元 再买 ${str}元 可减 ${item.reduce}元 [去凑单]`
+            this.changeReduceFeeDataMut(perItem.reduce);
+            return `已减${perItem.reduce}元 再买 ${str}元 可减 ${
+              item.reduce
+            }元 [去凑单]`;
           }
         } else {
-          continue
+          continue;
         }
       }
-      var lastItem = activity_info[activity_info.length - 1]
-      this.changeReduceFeeDataMut(lastItem.priceLower)
-      return `已满 ${lastItem.priceLower} 可减 ${lastItem.reduce}`
+      var lastItem = activity_info[activity_info.length - 1];
+      this.changeReduceFeeDataMut(lastItem.priceLower);
+      return `已满 ${lastItem.priceLower} 可减 ${lastItem.reduce}`;
     },
     btnTitle() {
       // var content = `${this.shopInfo.min_price}元起送`
-      var content = '$15元起送'
-      var price = 0
+      var content = "$15元起送";
+      var price = 0;
       // this.foods.map(item => price += item.totalPrice)
-      if (price <= 0) return content
+      if (price <= 0) return content;
       if (price < this.shopInfo.min_price) {
-        var value = parseFloat(this.shopInfo.min_price - price).toFixed(1)
-        return `还差${value}元`
+        var value = parseFloat(this.shopInfo.min_price - price).toFixed(1);
+        return `还差${value}元`;
       } else {
-        return '去结算'
+        return "去结算";
       }
     }
   },
@@ -229,19 +341,14 @@ export default {
         openId: this.openId
       });
       this.info = data.info;
+      this.allcomments = data.allcomments;
+      this.goodcomm = data.goodcomm;
+      this.badcomm = data.badcomm;
       console.log(data);
     },
 
-    showToggle() {
-      // this.showDetail = !this.showDetail
-      // if (this.showDetail) {
-      //   this.$nextTick(() => {
-      //     this._initScroll()
-      //   })
-      // }
-      wx.navigateBack({
-        delta: 1
-      });
+    showType() {
+      this.showpop = !this.showpop;
     },
 
     _initScroll() {
@@ -252,6 +359,29 @@ export default {
       } else {
         this.detailWrapper.refresh();
       }
+    },
+
+    add () {
+      this.number += 1;
+    },
+
+    reduce () {
+      if (this.number > 0) {
+        this.number -= 1;
+      } else {
+        return false;
+      }
+    },
+
+    good (index) {
+      this.gooddisplay = 1
+      this.baddisplay = 0
+      this.alldisplay = 0
+    },
+    bad (index) {
+      this.baddisplay = 1
+      this.gooddisplay = 0
+      his.alldisplay = 0
     },
 
     addCart(event) {
